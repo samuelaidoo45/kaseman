98cf4fb58cbc5b890f4ca61d3f7beac79aebe371
SamA <45714641+samuelaidoo45@users.noreply.github.com>
2023-10-16 14:48:30 +0000

    update

--- views/loan_classification.py
+++ views/loan_classification.py
@@ -1,5 +1,5 @@
 # views/main_views.py
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template,request
 
 bp = Blueprint('loan_classification', __name__)
 
@@ -7,3 +7,12 @@ bp = Blueprint('loan_classification', __name__)
 def loan_classification():
     return render_template('test_1.html')
     
+@bp.route('/create_loan_classification',methods=['POST'])
+def create_loan_classification():
+    loan_class_name = request.form.get('loanClassName')
+    loan_class_desc = request.form.get('loanClassDesc')
+    
+    print("Loan Class Name:",loan_class_name)
+    print("Loan Class Description:",loan_class_desc)
+
+    return render_template('index.html')
