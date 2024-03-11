bb8102d41872b591dd342f566d572b42611ecfeb
SamA <45714641+samuelaidoo45@users.noreply.github.com>
2024-03-11 22:52:50 +0000

    update

--- setupenv.py
+++ setupenv.py
@@ -1,15 +1,16 @@
+import os
 import subprocess
 
-def run_passenger(app_script, port=3000):
+def run_passenger(port=3000):
     try:
-        # Use subprocess to run the app.py script with the built-in Python HTTP server
-        subprocess.run(["python", "-m", "http.server", str(port)], check=True, cwd=app_script)
+        # Get the current script directory
+        script_directory = os.path.dirname(os.path.realpath(__file__))
+
+        # Use subprocess to run the run.py script with "python run.py"
+        subprocess.run(["python", "run.py", str(port)], check=True, cwd=script_directory)
     except subprocess.CalledProcessError as e:
-        print(f"Error running app.py: {e}")
+        print(f"Error running run.py: {e}")
 
 if __name__ == "__main__":
-    # Replace with the actual path to your app.py script
-    app_script_path = "./run.py"
-
     # Specify the port (optional, defaults to 3000)
-    run_passenger(app_script_path, port=8000)
+    run_passenger(port=8000)
