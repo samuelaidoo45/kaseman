cdeba6fd1bbfd61af805b14813a2ca4f19901e4c
SamA <45714641+samuelaidoo45@users.noreply.github.com>
2024-03-12 00:13:11 +0000

    update

--- run.py
+++ run.py
@@ -4,7 +4,16 @@ from models import loan_classification, loan_classification_files, loan_classifi
 
 app = create_app()
 
+def find_free_port():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(('127.0.0.1', 0))
+    _, port = s.getsockname()
+    s.close()
+    return port
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = find_free_port()
+    app.run(port=port)
+
 
 
